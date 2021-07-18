7ad6699d1a6e2cf2b9592529e6ee807699c30020
Joshua Tait Bourke <jbou0010@student.monash.edu>
2021-07-19 03:58:01 +1000

    add: add some basic styled components to frontend

---
+++ client/src/components/MainLayout/MainLayout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+// TODO: Delete after
+import Header from '../Header/Header';
+
+
+export default function MainLayout() {
+    return (
+        <div className="screen-section">
+            <div className="content-section">
+                <Header text="UltraUrl" />
+            </div>
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainLayout from './components/MainLayout/MainLayout';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
