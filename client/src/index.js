e14205dbf21d6da8945171cd28a666bdc3fee053
Joshua Tait Bourke <jbou0010@student.monash.edu>
2021-07-21 19:30:39 +1000

    feat: client can send a request to server and returns new code

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LinkContainer from './components/LinkContainer/LinkContainer';
 import MainLayout from './components/MainLayout/MainLayout';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainLayout />
+    <MainLayout>
+      <LinkContainer />
+    </MainLayout>
   </React.StrictMode>,
   document.getElementById('root')
 );
