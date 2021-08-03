de83b2fb77ab65484a0642de8c73f9c1d67f4d9a
Joshua Tait Bourke <jbou0010@student.monash.edu>
2021-08-04 02:16:51 +1000

    feat: Add page for fetching created urls

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LinkContainer from './components/LinkContainer/LinkContainer';
 import MainLayout from './components/MainLayout/MainLayout';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './index.css';
+import RedirectContainer from './components/RedirectContainer/RedirectContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <MainLayout>
-      <LinkContainer />
+    <Router>
+        <Route exact path='/' component={LinkContainer} />
+        <Route path='/:redirectid' component={RedirectContainer} />
+
+      </Router>
     </MainLayout>
   </React.StrictMode>,
   document.getElementById('root')
